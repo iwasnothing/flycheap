f367d6c53b3adb7015a5cd47e30a33550a77f40f
Ka Hing leung <iwasnothing@gmail.com>
2018-12-27 11:01:41 +0800

    delete.sh

--- jmeter/startjob.sh
+++ jmeter/startjob.sh
@@ -9,6 +9,7 @@ python gen_parm.py genall 30
 N=`wc -l parm.csv|awk '{print $1}'`
 /opt/apache-jmeter-5.0/bin/jmeter -n -t  /app/flycheap/jmeter/hkexpress.jmx -JLOOPCNT=$N -JDIR=$HOMEDIR
 echo "test completed"
+cat /opt/apache-jmeter-5.0/bin/jmeter.log
 ls |grep flight|grep json|wc -l
 echo "upload"
 ls|grep flycheap|grep firebase|grep adminsdk|grep json|xargs rm -f
