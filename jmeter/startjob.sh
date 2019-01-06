0d13ff89567c59aff60322d3ff687497a479008f
Ka Hing leung <iwasnothing@gmail.com>
2019-01-06 22:09:23 +0800

    longer

--- jmeter/startjob.sh
+++ jmeter/startjob.sh
@@ -5,7 +5,7 @@ cd $HOMEDIR
 
 ls |grep flight|grep json|xargs rm -f
 echo "gen parm.csv"
-python gen_parm.py genall 30
+python gen_parm.py genall 90
 N=`wc -l parm.csv|awk '{print $1}'`
 /opt/apache-jmeter-5.0/bin/jmeter -n -t  /app/flycheap/jmeter/hkexpress.jmx -JLOOPCNT=$N -JDIR=$HOMEDIR
 echo "test completed"
